050e751eaea5479f0748ec6aded4cf734db8c0ac
sherkhan <sherkhan1167746@gmail.com>
2024-01-18 15:43:42 +0500

    add total sum sentence

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ for filepath in filepaths:
     pdf.cell(w=35, h=8, border=1, txt=column[2])
     pdf.cell(w=30, h=8, border=1, txt=column[3])
     pdf.cell(w=30, h=8, border=1, txt=column[4], ln=1)
-    sum = df[""]
+
     for index, row in df.iterrows():
         pdf.set_font(family="Times",size=10)
         pdf.set_text_color(80, 80, 80)
@@ -38,6 +38,15 @@ for filepath in filepaths:
         pdf.cell(w=35, h=8, border=1, txt=str(row["amount_purchased"]))
         pdf.cell(w=30, h=8, border=1,txt=str(row["price_per_unit"]))
         pdf.cell(w=30, h=8, border=1,txt=str(row["total_price"]), ln=1)
+    sum = df["total_price"].sum()
+    pdf.cell(w=30, h=8, border=1, txt="")
+    pdf.cell(w=60, h=8, border=1, txt="")
+    pdf.cell(w=35, h=8, border=1, txt="")
+    pdf.cell(w=30, h=8, border=1, txt="")
+    pdf.set_font(family="Times", size=10,style="b")
+    pdf.cell(w=30, h=8, border=1, txt=str(sum), ln=1)
 
+    pdf.set_font(family="Times", size=10,style="b")
+    pdf.cell(w=30, h=10, txt=f"The total due sum is {sum}")
     pdf.output(f"PDFs/{filename}.pdf")
 
